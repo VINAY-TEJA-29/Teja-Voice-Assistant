b5f71996ea64ecadef3fb9f26c85cddb0affb023
VINAY TEJA <vinayteja292005@gmail.com>
2025-06-29 19:23:14 +0530

    Update teja_assistant_gui.py

--- teja_assistant_gui.py
+++ teja_assistant_gui.py
@@ -13,7 +13,7 @@ engine = pyttsx3.init()
 engine.setProperty("rate", 150)
 
 # OpenAI API key
-openai.api_key = "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"
+openai.api_key = "sk-proj-kEwtVV6XZCFvU1TA"
 
 # Functions
 def speak(text):
